89833f752b155b4c907fb25efc2de47e7ac6774d
Vishakha ojha <vishakhaojha57@gmail.com>
2026-09-10 00:16:04 +0530

    your commit to memory changes

--- memory/conversation_memory.py
+++ memory/conversation_memory.py
@@ -1,7 +1,17 @@
-from langchain_classic.memory import ConversationBufferMemory
+from langchain.memory import ConversationBufferMemory
+from langchain_community.chat_message_histories import FileChatMessageHistory
+import os
 
+HISTORY_FILE = "history/chat_history.json"
 def get_memory():
+    os.makedirs(os.path.dirname(HISTORY_FILE), exist_ok=True)
+
+    message_history = FileChatMessageHistory(file_path=HISTORY_FILE)
+    
     return ConversationBufferMemory(
-        memory_key = "chat_history",
-        return_messages = True
+        memory_key="chat_history",
+        chat_memory=message_history, 
+        return_messages=True
     )
+
+    
